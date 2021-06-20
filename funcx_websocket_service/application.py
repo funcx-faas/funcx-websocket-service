e8f1731d8e23c557c1eedc8974d24f0dd2a06a56
Loonride <22580625+Loonride@users.noreply.github.com>
2021-06-20 15:28:20 -0500

    add setup script and version to turn into package

--- funcx_websocket_service/application.py
+++ funcx_websocket_service/application.py
@@ -1,11 +1,27 @@
 import os
+import argparse
 import logging
 from funcx_websocket_service.server import WebSocketServer
+from funcx_websocket_service.utils.loggers import set_stream_logger
 
-logger = logging.getLogger(__name__)
+def cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", action='store_true',
+                        help="Debug logging")
+
+    args = parser.parse_args()
+
+    env_debug = os.environ.get('FUNCX_WEBSOCKET_SERVICE_DEBUG')
+
+    debug = args.debug is True or env_debug is not None
+    set_stream_logger(level=logging.DEBUG if debug else logging.INFO)
+
+    run()
 
 
 def run():
+    logger = logging.getLogger(__name__)
+
     REDIS_HOST = os.environ.get('REDIS_HOST')
     if not REDIS_HOST:
         REDIS_HOST = os.environ.get('FUNCX_REDIS_MASTER_SERVICE_HOST')
@@ -32,3 +48,6 @@ def run():
     logger.debug(f'Using redis host: {REDIS_HOST}, redis port: {REDIS_PORT}, RabbitMQ host: {RABBITMQ_HOST}, web service URI: {WEB_SERVICE_URI}')
 
     WebSocketServer(REDIS_HOST, REDIS_PORT, RABBITMQ_HOST, WEB_SERVICE_URI)
+
+if __name__ == '__main__':
+    cli()
---
+++ funcx_websocket_service/version.py
@@ -0,0 +1,7 @@
+"""Set module version.
+
+<Major>.<Minor>.<maintenance>[alpha/beta/..]
+Alphas will be numbered like this -> 0.4.0a0
+"""
+VERSION = '0.2.1'
+MIN_SDK_VERSION = '0.2.1'
