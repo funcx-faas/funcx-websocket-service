980c92c8742405860da2811f2ad69bc2334af13c
Loonride <22580625+Loonride@users.noreply.github.com>
2021-06-30 18:00:08 -0500

    add basic health test and integrate into docker build

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,10 @@ RUN pip install -U pip
 COPY . /opt/funcx-websocket-service
 RUN pip install .
 
+# run tests
+RUN pip install -r test-requirements.txt
+RUN pytest funcx_websocket_service/tests
+
 USER uwsgi
 EXPOSE 6000
 
---
+++ funcx_websocket_service/tests/conftest.py
@@ -0,0 +1,17 @@
+import os
+import signal
+import time
+from subprocess import Popen
+import pytest
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+root_path = os.path.abspath(os.path.join(dir_path, '../..'))
+
+@pytest.fixture(autouse=True, scope="session")
+def run_server():
+    # See: https://stackoverflow.com/questions/4789837/how-to-terminate-a-python-subprocess-launched-with-shell-true
+    process = Popen(['bash', 'entrypoint.sh'], cwd=root_path, preexec_fn=os.setsid)
+    time.sleep(5)
+    yield
+    os.killpg(os.getpgid(process.pid), signal.SIGTERM)
