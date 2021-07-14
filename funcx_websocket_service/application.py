181c65e1edf4c051cad45589f380ed7528b07187
Loonride <22580625+Loonride@users.noreply.github.com>
2021-07-14 16:15:37 -0500

    log all exceptions as json

--- funcx_websocket_service/application.py
+++ funcx_websocket_service/application.py
@@ -4,45 +4,51 @@ import logging
 from funcx_websocket_service.server import WebSocketServer
 from funcx_websocket_service.utils.loggers import set_stream_logger
 
+logger = logging.getLogger(__name__)
+
 
 def cli():
     """CLI entrypoint for WebSocket server that takes CLI args, gathers env vars,
     and starts the WebSocket server
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--debug", action='store_true',
-                        help="Debug logging")
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-d", "--debug", action='store_true',
+                            help="Debug logging")
 
-    args = parser.parse_args()
+        args = parser.parse_args()
 
-    env_debug = os.environ.get('FUNCX_WEBSOCKET_SERVICE_DEBUG')
+        env_debug = os.environ.get('FUNCX_WEBSOCKET_SERVICE_DEBUG')
 
-    debug = args.debug is True or env_debug is not None
-    logger = set_stream_logger(level=logging.DEBUG if debug else logging.INFO)
+        debug = args.debug is True or env_debug is not None
+        global logger
+        logger = set_stream_logger(level=logging.DEBUG if debug else logging.INFO)
 
-    REDIS_HOST = os.environ.get('REDIS_HOST')
-    if not REDIS_HOST:
-        REDIS_HOST = os.environ.get('FUNCX_REDIS_MASTER_SERVICE_HOST')
-    REDIS_PORT = os.environ.get('REDIS_PORT')
-    RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST')
-    if not RABBITMQ_HOST:
-        RABBITMQ_HOST = os.environ.get('FUNCX_RABBITMQ_SERVICE_HOST')
+        REDIS_HOST = os.environ.get('REDIS_HOST')
+        if not REDIS_HOST:
+            REDIS_HOST = os.environ.get('FUNCX_REDIS_MASTER_SERVICE_HOST')
+        REDIS_PORT = os.environ.get('REDIS_PORT')
+        RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST')
+        if not RABBITMQ_HOST:
+            RABBITMQ_HOST = os.environ.get('FUNCX_RABBITMQ_SERVICE_HOST')
 
-    WEB_SERVICE_URI = os.environ.get('WEB_SERVICE_URI')
+        WEB_SERVICE_URI = os.environ.get('WEB_SERVICE_URI')
 
-    if REDIS_HOST is None:
-        REDIS_HOST = '127.0.0.1'
+        if REDIS_HOST is None:
+            REDIS_HOST = '127.0.0.1'
 
-    if REDIS_PORT is None:
-        REDIS_PORT = 6379
+        if REDIS_PORT is None:
+            REDIS_PORT = 6379
 
-    if RABBITMQ_HOST is None:
-        RABBITMQ_HOST = '127.0.0.1'
+        if RABBITMQ_HOST is None:
+            RABBITMQ_HOST = '127.0.0.1'
 
-    if WEB_SERVICE_URI is None:
-        WEB_SERVICE_URI = 'http://127.0.0.1:5000'
+        if WEB_SERVICE_URI is None:
+            WEB_SERVICE_URI = 'http://127.0.0.1:5000'
 
-    logger.info('Starting WebSocket Server')
-    logger.debug(f'Using redis host: {REDIS_HOST}, redis port: {REDIS_PORT}, RabbitMQ host: {RABBITMQ_HOST}, web service URI: {WEB_SERVICE_URI}')
+        logger.info('Starting WebSocket Server')
+        logger.debug(f'Using redis host: {REDIS_HOST}, redis port: {REDIS_PORT}, RabbitMQ host: {RABBITMQ_HOST}, web service URI: {WEB_SERVICE_URI}')
 
-    WebSocketServer(REDIS_HOST, REDIS_PORT, RABBITMQ_HOST, WEB_SERVICE_URI)
+        WebSocketServer(REDIS_HOST, REDIS_PORT, RABBITMQ_HOST, WEB_SERVICE_URI)
+    except Exception:
+        logger.exception('An exception occurred while starting the server')
