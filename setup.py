4c2f891cb0c4036658171f3c7713eb2709bced28
Stephen Rosen <sirosen@globus.org>
2021-11-23 18:34:18 +0000

    Setup new tox and pre-commit config
    Update GitHub Actions to use the new config.
    
    Move requirements data into `setup.py` so that `tox` can install the
    package. (Using `requirements.txt` does not make a well-formed package
    unless it is also present in a manifest or package data.)

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
 import os
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 
 version_ns = {}
 with open(os.path.join("funcx_websocket_service", "version.py")) as f:
     exec(f.read(), version_ns)
-version = version_ns['VERSION']
-print("Version : ", version)
+version = version_ns["VERSION"]
 
-with open('requirements.txt') as f:
-    install_requires = f.readlines()
+
+REQUIRES = [
+    "aioredis>=2.0.0",
+    "aiohttp",
+    "websockets",
+    "aio-pika",
+    "python-json-logger",
+    "funcx-common[redis,boto3]==0.0.9",
+    "redis==3.5.3",
+]
 
 setup(
-    name='funcx_websocket_service',
+    name="funcx_websocket_service",
     version=version,
     packages=find_packages(),
-    description='funcX WebSocket Service: High Performance Function Serving for Science',
-    install_requires=install_requires,
+    description="funcX WebSocket Service: High Performance Function Serving for Science",
+    install_requires=REQUIRES,
     python_requires=">=3.6",
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -24,19 +32,16 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering"
+        "Topic :: Scientific/Engineering",
     ],
-    keywords=[
-        "funcX",
-        "FaaS",
-        "Function Serving"
-    ],
-    entry_points={'console_scripts':
-                  ['websocket-service=funcx_websocket_service.application:cli',
-                  ]
+    keywords=["funcX", "FaaS", "Function Serving"],
+    entry_points={
+        "console_scripts": [
+            "websocket-service=funcx_websocket_service.application:cli",
+        ]
     },
-    author='funcX team',
-    author_email='labs@globus.org',
+    author="funcX team",
+    author_email="labs@globus.org",
     license="Apache License, Version 2.0",
-    url="https://github.com/funcx-faas/funcx"
+    url="https://github.com/funcx-faas/funcx",
 )
